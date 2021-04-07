91d5496b157bad0acfabe7450d7db27fc3bd60f7
Dima Kozitsyn <dmytrokozitsyn@gmail.com>
2021-04-07 17:52:39 +0300

    добавляет задание

---
+++ script.js
@@ -0,0 +1,52 @@
+const startRef = document.querySelector('[data-action="start"]');
+const stopRef = document.querySelector('[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+
+startRef.addEventListener('click', starting)
+stopRef.addEventListener('click', stoping)
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+ const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let setIntervalId;
+
+function starting() {
+    startRef.setAttribute('disabled', true)
+    return setIntervalId = setInterval(colorChenging,1000)    
+}
+
+function stoping() {
+    startRef.removeAttribute('disabled')
+    clearInterval(setIntervalId);
+}
+
+function colorChenging() {
+    const randomNum = randomIntegerFromInterval(0, 5);
+    const color = colors[randomNum];
+    bodyRef.style.backgroundColor = `${color}`;
+ }
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
